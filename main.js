be529bdcff191a85e516bb323cb6ade8cbd8164a
tanabee <tanabee1986@gmail.com>
2019-04-29 14:03:01 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,47 @@
+var TOKEN   = PropertiesService.getScriptProperties().getProperty("TOKEN");
+var CHANNEL = PropertiesService.getScriptProperties().getProperty("CHANNEL");
+
+function main() {
+  const baseUrl = 'https://slack.com/api/channels.history';
+  const baseParameters = [
+    'token=' + TOKEN,
+    'channel=' + CHANNEL,
+    'count=1000',
+  ];
+
+  var messages = [];
+  var latestMessage = '';
+  do {
+    var parameters = baseParameters.concat([latestMessage]).join('&');
+    var res = fetchSlackChannelMessages(baseUrl + '?' + parameters);
+    var newMessages = res.messages
+      .filter(function (v) {
+        return 'reply_count' in v;
+      }).map(function (v) {
+        return [
+          v.client_msg_id,
+          v.type,
+          v.text,
+          v.user,
+          v.ts,
+          v.reply_count,
+          v.reply_users_count,
+        ];
+      });
+    messages = messages.concat(newMessages);
+    latestMessage = 'latest=' + res.messages[res.messages.length-1].ts;
+  } while (res.has_more);
+
+  SpreadsheetApp
+    .getActiveSheet()
+    .getRange('A2:G' + (messages.length+1))
+    .setValues(messages);
+}
+
+function fetchSlackChannelMessages(url) {
+	const res = UrlFetchApp.fetch(url, {
+		method: 'GET',
+		headers: { "Content-Type": 'application/json' }
+	});
+  return JSON.parse(res);
+}
